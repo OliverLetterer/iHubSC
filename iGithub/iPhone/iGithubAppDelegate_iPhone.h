d54b41cb9b3e4960f269d9ca93274914f7c01342
docmorelli <oliver@Oliver-Letterers-MacBook-Air.local>
2011-07-17 16:20:54 +0200

    added todo warning

--- iGithub/iPhone/iGithubAppDelegate_iPhone.h
+++ iGithub/iPhone/iGithubAppDelegate_iPhone.h
@@ -14,6 +14,8 @@
 #import "GHUserViewController.h"
 #import "GHSearchViewController.h"
 
+#warning show news feed times
+
 @interface iGithubAppDelegate_iPhone : iGithubAppDelegate <MFMailComposeViewControllerDelegate> {
     UITabBarController *_tabBarController;
     GHOwnerNewsFeedViewController *_newsFeedViewController;
