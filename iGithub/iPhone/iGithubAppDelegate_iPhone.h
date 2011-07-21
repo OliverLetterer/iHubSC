1d3d8e1c99aa2dcd09c1c3a27a7ee2e59a4b1ce7
docmorelli <oliver@Oliver-Letterers-MacBook-Air.local>
2011-07-21 20:44:08 +0200

    updated todo warning

--- iGithub/iPhone/iGithubAppDelegate_iPhone.h
+++ iGithub/iPhone/iGithubAppDelegate_iPhone.h
@@ -14,6 +14,7 @@
 #import "GHUserViewController.h"
 #import "GHSearchViewController.h"
 
+#warning rename iPhone tableViewCells
 #warning Icons for folders and files in browse content
 #warning Update news feed cell for push payload to use one label
 #warning adopt heightForContent on iPhone
