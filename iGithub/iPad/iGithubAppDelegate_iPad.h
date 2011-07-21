a7b70649ef5314d0f532e79c6e7f9e9ebbda25c2
docmorelli <oliver@Oliver-Letterers-MacBook-Air.local>
2011-07-21 19:15:40 +0200

    added warning

--- iGithub/iPad/iGithubAppDelegate_iPad.h
+++ iGithub/iPad/iGithubAppDelegate_iPad.h
@@ -10,6 +10,8 @@
 #import "iGithubAppDelegate.h"
 #import "ANAdvancedNavigationController.h"
 
+#warning update Default.png to match new state
+
 @interface iGithubAppDelegate_iPad : iGithubAppDelegate <ANAdvancedNavigationControllerDelegate> {
     ANAdvancedNavigationController *_advancedNavigationController;
 }
