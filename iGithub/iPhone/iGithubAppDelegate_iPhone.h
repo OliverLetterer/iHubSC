19bbbdc077f3d7a75a711143308f96331c670c9d
docmorelli <oliver@Oliver-Letterers-MacBook-Air.local>
2011-07-21 10:22:00 +0200

    updated Default.png's for iPhone to play nicely with serialization

--- iGithub/iPhone/iGithubAppDelegate_iPhone.h
+++ iGithub/iPhone/iGithubAppDelegate_iPhone.h
@@ -15,7 +15,6 @@
 #import "GHSearchViewController.h"
 
 #warning save expansion states
-#warning update default.pngs (make 2 new defaults)
 #warning Icons for folders and files in browse content
 #warning Update news feed cell for push payload to use one label
 #warning adopt heightForContent on iPhone
