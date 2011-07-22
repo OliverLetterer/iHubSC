39650ab9c7a7ed3a4c9c86f2e2b8d30f960f1dd9
docmorelli <oliver@Oliver-Letterers-MacBook-Air.local>
2011-07-22 12:12:26 +0200

    removed some warnings

--- iGithub/iPhone/iGithubAppDelegate_iPhone.h
+++ iGithub/iPhone/iGithubAppDelegate_iPhone.h
@@ -14,10 +14,7 @@
 #import "GHUserViewController.h"
 #import "GHSearchViewController.h"
 
-#warning rename iPhone tableViewCells
 #warning Icons for folders and files in browse content
-#warning Update news feed cell for push payload to use one label
-#warning adopt heightForContent on iPhone
 #warning Dttextview selectionstyle and userinteraction
 
 @interface iGithubAppDelegate_iPhone : iGithubAppDelegate <MFMailComposeViewControllerDelegate> {
