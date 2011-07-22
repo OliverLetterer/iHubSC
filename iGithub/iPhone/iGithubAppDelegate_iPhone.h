b7890048bf6bb47224a8b05e31048e5e20c64e7c
docmorelli <oliver@Oliver-Letterers-MacBook-Air.local>
2011-07-22 16:48:30 +0200

    added todo warning

--- iGithub/iPhone/iGithubAppDelegate_iPhone.h
+++ iGithub/iPhone/iGithubAppDelegate_iPhone.h
@@ -14,6 +14,7 @@
 #import "GHUserViewController.h"
 #import "GHSearchViewController.h"
 
+#warning Increase fonts
 #warning Icons for folders and files in browse content
 #warning Dttextview selectionstyle and userinteraction
 
