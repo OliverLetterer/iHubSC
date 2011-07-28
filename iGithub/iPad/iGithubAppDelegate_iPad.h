21797d9f69f7e0a8c422c8b558a83b913be8c4f0
Oliver Letterer <oliver.letterer@googlemail.com>
2011-07-28 16:06:53 +0200

    removed todo warning

--- iGithub/iPad/iGithubAppDelegate_iPad.h
+++ iGithub/iPad/iGithubAppDelegate_iPad.h
@@ -10,8 +10,6 @@
 #import "iGithubAppDelegate.h"
 #import "ANAdvancedNavigationController.h"
 
-#warning make keychain submodule?
-
 @interface iGithubAppDelegate_iPad : iGithubAppDelegate <ANAdvancedNavigationControllerDelegate> {
     ANAdvancedNavigationController *_advancedNavigationController;
 }
