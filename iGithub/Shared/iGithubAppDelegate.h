3b30bfbf48dd33072d24f5e77d28dea67dd85faa
Oliver Letterer <oliver.letterer@googlemail.com>
2011-07-28 18:06:47 +0200

    added todo warning

--- iGithub/Shared/iGithubAppDelegate.h
+++ iGithub/Shared/iGithubAppDelegate.h
@@ -11,6 +11,7 @@
 
 #warning better organization support
 #warning search background does not match new style
+#warning attributed cell's look strange
 
 @interface iGithubAppDelegate : NSObject <UIApplicationDelegate> {
     NSManagedObjectContext *_managedObjectContext;
