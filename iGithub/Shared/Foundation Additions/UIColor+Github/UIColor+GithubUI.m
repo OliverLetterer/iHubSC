739f1b8635a1694e658f1ac729414ee35ba079e5
Oliver Letterer <oliver.letterer@googlemail.com>
2011-08-09 10:53:19 +0200

    made double floats

--- iGithub/Shared/Foundation Additions/UIColor+Github/UIColor+GithubUI.m
+++ iGithub/Shared/Foundation Additions/UIColor+Github/UIColor+GithubUI.m
@@ -15,7 +15,7 @@
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
         return [UIColor darkGrayColor];
     }
-    return [UIColor colorWithRed:22.0/255.0f green:70.0/255.0f blue:110.0/255.0f alpha:1.0];
+    return [UIColor colorWithRed:22.0f/255.0f green:70.0f/255.0f blue:110.0f/255.0f alpha:1.0f];
 }
 
 @end
