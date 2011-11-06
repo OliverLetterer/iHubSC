e406d1e03cb55dc37ee90e834ae4b9a4dc36a392
Oliver Letterer <oliver.letterer@googlemail.com>
2011-11-06 20:17:59 +0100

    Removes obsolete CoreData store. #111

--- iGithub/Shared/iGithubAppDelegate.h
+++ iGithub/Shared/iGithubAppDelegate.h
@@ -10,22 +10,13 @@
 #import <CoreData/CoreData.h>
 
 @interface iGithubAppDelegate : NSObject <UIApplicationDelegate> {
-    NSManagedObjectContext *_managedObjectContext;
-    NSManagedObjectModel *_managedObjectModel;
-    NSPersistentStoreCoordinator *_persistentStoreCoordinator;
+    
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 
-@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
-@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
-@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
 @property (nonatomic, readonly) NSMutableDictionary *serializedStateDictionary;
 @property (nonatomic, readonly) NSString *lastKnownApplicationStateDictionaryFilePath;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
 
 - (void)setupAppearences;
 
